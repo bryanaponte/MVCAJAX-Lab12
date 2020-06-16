c262e922342ab98c743a7e63d6966db196363e27
bryanaponte <bryan.aponte@tecsup.edu.pe>
2020-06-15 20:36:06 -0500

    Agregar archivos de proyecto.

---
+++ DOMAIN/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOMAIN
+{
+    public class Student
+    {
+        [Key]
+        public int studentID { get; set; }
+        [Required]
+        public string studentName { get; set; }
+        [Required]
+        public string studentAddress { get; set; }
+
+    }
+}
---
+++ MVCAJAX/Controllers/StudentController.cs
@@ -0,0 +1,50 @@
+﻿using MVCAJAX.Models;
+using System;
+using DOMAIN;
+using SERVICE;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MVCAJAX.Controllers
+{
+    public class StudentController : Controller
+    {
+        private StudentService service = new StudentService();
+        // Get: Student
+
+        public ActionResult IndexRazor()
+        {
+            var model = (from c in service.Get()
+                         select new StudentModel
+                         {
+                             ID = c.studentID,
+                             Address = c.studentAddress,
+                             Name = c.studentName
+
+                         }).ToList();
+            return View(model);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getStudent(string id) 
+        {
+            return Json(service.Get(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult createStudent(Student std)
+        {
+            service.Insert(std);
+            string message = "SUCCESS";
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+        }
+        
+    }
+}
